0c6ad30a990cf810fe939500d67ce38c86f63634
Rajputo07Rohit <rajputrohit2kk@gmail.com>
2025-11-20 16:44:32 +0530

    integrated login api in login screen

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("hima@gmail.com");
+  const [password, setPassword] = useState("Hima@123");
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className=" flex justify-center my-10 ">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          {/* Title */}
+          <h2 className="text-2xl font-bold text-center text-base-content">
+            Welcome Back 👋
+          </h2>
+          <h2 className="card-title justify-center">Login!</h2>
+          <div>
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input rounded-md"
+                placeholder="Type here"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                className="input rounded-md"
+                placeholder="Type here"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button
+              className="btn btn-primary my-2 rounded-xl"
+              onClick={handleSubmit}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
